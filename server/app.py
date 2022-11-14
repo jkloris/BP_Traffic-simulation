de98f67994b29bd7de56f81d89fb3ae890511947
jkloris <kapusta.jergus@gmail.com>
2022-11-14 14:07:55 +0100

    zakladna struktura

---
+++ server/app.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+
+import asyncio
+import itertools
+import json
+from time import sleep
+from turtle import position
+import sumolib
+from sumolib import checkBinary
+import traci
+import websockets
+
+
+import os
+import sys
+import optparse
+
+
+# we need to import some python modules from the $SUMO_HOME/tools directory
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+
+async def handler(websocket):
+    async for message in websocket:
+        # Parse a "play" event from the UI.
+        event = json.loads(message)
+        if event["type"] == "start":
+            await traciStart(websocket)
+        if event["type"] == "play":
+            await traciSimStep(websocket)
+
+
+# contains TraCI control loop
+
+async def traciStart(websocket):
+    sumoBinary = checkBinary('sumo')
+
+    traci.start([sumoBinary, "-c", "..\sumo\demoAAA.sumocfg",
+                "--tripinfo-output", "..\sumo\_tripinfo.xml"])
+
+    print(traci)
+
+    await run(websocket)
+
+
+async def run(websocket):
+    step = 0
+    print("::::::::::::RUN TRACI\n")
+    vehicleData = getVehicles()
+
+    while traci.simulation.getMinExpectedNumber() > 0:
+
+        await traciSimStep(websocket, vehicleData)
+        await asyncio.sleep(30 / 1000)
+    traci.close()
+    sys.stdout.flush()
+
+
+def getVehicles():
+    vehicleIDs = traci.vehicle.getIDList()
+    vehicleData = {"removed": [], "added": vehicleIDs,
+                   "data": {}, "all": vehicleIDs}
+
+    for id in vehicleIDs:
+        pos = traci.vehicle.getPosition(id)
+        angle = traci.vehicle.getAngle(id)
+        vehicleData["added"][id] = {"position": pos, "angle": angle}
+    return vehicleData
+
+
+def updateVehicles(vehicleData):
+    vehicleIDs = traci.vehicle.getIDList()
+    vehicleData["removed"] = traci.simulation.getArrivedIDList()
+    vehicleData["added"] = traci.simulation.getDepartedIDList()
+
+    for i in vehicleData["removed"]:
+        vehicleData["all"] = [item for item in vehicleData["all"] if item != i]
+
+    vehicleData["all"] += vehicleData["added"]
+
+    for id in vehicleIDs:
+        pos = traci.vehicle.getPosition(id)
+        angle = traci.vehicle.getAngle(id)
+        vehicleData["data"][id] = {"position": pos, "angle": angle}
+
+    return vehicleData
+
+
+async def traciSimStep(websocket, vehicleData):
+
+    vehicleData = updateVehicles(vehicleData)
+    traci.simulationStep()
+
+    time = traci.simulation.getTime()
+    print("_______________________\n", time)
+    await websocket.send(json.dumps(vehicleData))
+
+
+async def main():
+    async with websockets.serve(handler, "", 8001):
+        await asyncio.Future()  # run forever
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ two/twotest.js
@@ -0,0 +1,63 @@
+var elem = document.getElementById("board");
+
+var centerX = window.innerWidth / 2;
+var centerY = window.innerHeight / 2;
+
+var two = new Two({ width: 0.95 * window.innerWidth, height: 1000 }).appendTo(elem);
+
+class Vehicle {
+  constructor(id, x, y) {
+    this.id = id;
+    this.width = 5;
+    this.height = 10;
+    this.obj = two.makeEllipse(x, y, this.width, this.height);
+    this.obj.fill = "#" + ((Math.random() * 0xffffff) << 0).toString(16);
+  }
+  move(x, y, angle) {
+    this.obj.translation.x = centerX + x;
+    this.obj.translation.y = centerY + y;
+    this.obj.rotation = -(Math.PI * angle) / 180;
+  }
+}
+
+vehicles = {};
+// for (i = 0; i < 130; i++) {
+//   cars.push(two.makeEllipse(Math.random() * two.width * 2 - two.width, centerY, 5, 10));
+//   console.log(cars[i]);
+//   cars[i].fill = "#" + ((Math.random() * 0xffffff) << 0).toString(16);
+// }
+
+function updateVehicleObjects(vehicleData) {
+  for (i in vehicleData.added) {
+    id = vehicleData.added[i];
+    pos = vehicleData.data[id].position;
+    vehicles[id] = new Vehicle(id, pos[0], pos[1]);
+  }
+  for (i in vehicleData.removed) {
+    id = vehicleData.removed[i];
+    two.remove(vehicles[id].obj);
+    delete vehicles[id];
+  }
+}
+
+function drawVehicles(vehicleData) {
+  for (i in vehicleData.all) {
+    id = vehicleData.all[i];
+    data = vehicleData.data[id];
+    vehicles[id].move(data.position[0], data.position[1], data.angle);
+  }
+  two.update();
+}
+
+// two
+//   .bind("update", function (frameCount) {
+//     for (i in cars) {
+//       // x = ;
+//       // y = cars[i].traslation.y;
+//       cars[i].translation.y = cars[.];
+//       // console.log(cars[i]);
+//     }
+//   })
+//   .play();
+
+document.addEventListener("click", function (event) {});
