48c6733a116e49200eff232c2e762fdb1e3f7a3a
jkloris <kapusta.jergus@gmail.com>
2023-02-10 17:30:51 +0100

    main class refactor

--- client/main.js
+++ client/main.js
@@ -1,89 +1,93 @@
-function showMessage(message) {
-	window.setTimeout(() => window.alert(message), 50);
-}
+class Main {
+	constructor(websocket, buttons) {
+		this.websocket = websocket;
+		this.receiveMsgs(websocket);
+		this.sendMsgs(buttons, websocket);
+	}
 
-function receiveMoves(websocket) {
-	websocket.addEventListener('message', ({ data }) => {
-		const event = JSON.parse(data);
+	receiveMsgs(websocket) {
+		websocket.addEventListener('message', ({ data }) => {
+			const event = JSON.parse(data);
 
-		// console.log(event);
-		switch (event.type) {
-			case 'step':
-				updateVehicleObjects(event.data);
-				drawVehicles(event.data);
-				network.drawTrafficLights(event.trafficLights);
-				break;
-			case 'network':
-				if (network == null) {
-					network = new Network(event.data, event.trafficLights, event.boundary, two);
-					network.draw();
-				}
-			case 'restart':
-				clearNetwork();
-				break;
-			default:
-				break;
-		}
-	});
-}
+			// console.log(event);
+			switch (event.type) {
+				case 'step':
+					updateVehicleObjects(event.data);
+					drawVehicles(event.data);
+					network.drawTrafficLights(event.trafficLights);
+					break;
+				case 'network':
+					if (network == null) {
+						network = new Network(event.data, event.trafficLights, event.boundary, two);
+						network.draw();
+					}
+				case 'restart':
+					clearNetwork();
+					break;
+				default:
+					break;
+			}
+		});
+	}
 
-function sendMoves(buttons, websocket) {
-	buttons['start'].onclick = async () => {
-		document.querySelector('#scenarios').style.display = 'none';
-		const event = {
-			type: 'start',
-			scenario: document.querySelector('#scenarios').selectedOptions[0].value,
+	sendMsgs(buttons, websocket) {
+		buttons['start'].onclick = async () => {
+			document.querySelector('#scenarios').style.display = 'none';
+			const event = {
+				type: 'start',
+				scenario: document.querySelector('#scenarios').selectedOptions[0].value,
+			};
+			websocket.send(JSON.stringify(event));
 		};
-		websocket.send(JSON.stringify(event));
-	};
 
-	buttons['pause'].onclick = async () => {
-		console.log('Click pause');
+		buttons['pause'].onclick = async () => {
+			console.log('Click pause');
 
-		const event = {
-			type: 'pause',
+			const event = {
+				type: 'pause',
+			};
+			await websocket.send(JSON.stringify(event));
 		};
-		await websocket.send(JSON.stringify(event));
-	};
 
-	buttons['play'].onclick = async () => {
-		console.log('Click play');
+		buttons['play'].onclick = async () => {
+			console.log('Click play');
 
-		const event = {
-			type: 'play',
+			const event = {
+				type: 'play',
+			};
+			await websocket.send(JSON.stringify(event));
 		};
-		await websocket.send(JSON.stringify(event));
-	};
-	//restart button
-	buttons['restart'].onclick = async () => {
-		console.log('Click restart');
+		//restart button
+		buttons['restart'].onclick = async () => {
+			console.log('Click restart');
 
-		const event = {
-			type: 'restart',
+			const event = {
+				type: 'restart',
+			};
+			await websocket.send(JSON.stringify(event));
 		};
-		await websocket.send(JSON.stringify(event));
-	};
 
-	// setSpeed button
-	buttons['setSpeed'].onclick = async () => {
-		var sliderVal = document.getElementById('speedSlider').value;
-		console.log(sliderVal);
-		const event = {
-			type: 'setSpeed',
-			value: sliderVal,
+		// setSpeed button
+		buttons['setSpeed'].onclick = async () => {
+			var sliderVal = document.getElementById('speedSlider').value;
+			console.log(sliderVal);
+			const event = {
+				type: 'setSpeed',
+				value: sliderVal,
+			};
+			await websocket.send(JSON.stringify(event));
 		};
-		await websocket.send(JSON.stringify(event));
-	};
-	// setScale button
-	buttons['setScale'].onclick = async () => {
-		var sliderVal = document.getElementById('scaleSlider').value;
-		const event = {
-			type: 'setScale',
-			value: sliderVal,
+		// setScale button
+		buttons['setScale'].onclick = async () => {
+			var sliderVal = document.getElementById('scaleSlider').value;
+			const event = {
+				type: 'setScale',
+				value: sliderVal,
+			};
+			console.log('scale');
+			await websocket.send(JSON.stringify(event));
 		};
-		console.log('scale');
-		await websocket.send(JSON.stringify(event));
-	};
+	}
 }
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -104,6 +108,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	};
 	// Open the WebSocket connection and register event handlers.
 	const websocket = new WebSocket('ws://localhost:8001/');
-	receiveMoves(websocket);
-	sendMoves(buttons, websocket);
+	// receiveMsgs(websocket);
+	// sendMsgs(buttons, websocket);
+	const main = new Main(websocket, buttons);
 });
